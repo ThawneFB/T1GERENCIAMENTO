7254926317f745fab7fe1e21de8368111ce17d50
Seu Nome <seuemail@exemplo.com>
2025-09-26 18:58:16 -0300

    oooooooo

---
+++ calc.py
@@ -0,0 +1,18 @@
+#######################################################
+####              C A L C U L A D O R A            ####
+#######################################################
+#### Prof. Filipo Mor - github.com/ProfessorFilipo ####
+#######################################################
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Erro: divisão por zero!"
+    return a / b

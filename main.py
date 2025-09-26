b33f11fdd59be763642f2b0eb968964761dda13b
Guilherme Frizzo Gardin <guigfrizzo@MacBook-Air-de-Guilherme-3.local>
2025-09-26 19:01:46 -0300

    verificacao simples

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-#######################################################
-####              C A L C U L A D O R A            ####
-#######################################################
-#### Prof. Filipo Mor - github.com/ProfessorFilipo ####
-#######################################################
-
 from calc import somar, subtrair, multiplicar, dividir
 
 def menu():
@@ -18,9 +12,18 @@ while True:
     menu()
     opcao = input("Escolha uma opção: ")
 
+    # verificação ridícula de simples
+    if not opcao.isdigit():
+        print("Digite apenas números do menu!")
+        continue
+
     if opcao == "0":
         print("Saindo...")
         break
+
+    if opcao not in ["1", "2", "3", "4"]:
+        print("Opção inválida!")
+        continue
 
     a = float(input("Digite o primeiro número: "))
     b = float(input("Digite o segundo número: "))
@@ -32,6 +35,7 @@ while True:
     elif opcao == "3":
         print("Resultado:", multiplicar(a, b))
     elif opcao == "4":
-        print("Resultado:", dividir(a, b))
-    else:
-        print("Opção inválida!")
+        if b == 0:
+            print("Não dá pra dividir por zero!")
+        else:
+            print("Resultado:", dividir(a, b))
